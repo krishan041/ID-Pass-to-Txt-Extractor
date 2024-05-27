b8930ccbd9af9124230cdad26c8ffb7e41d21278
Stubborn <131737791+stubbornlove@users.noreply.github.com>
2024-05-27 18:13:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 29707621
-api_hash = "a1f31eeb31a0b9e36f551da6b8b85752"
-bot_token = "6634971116:AAGhU6IQBREjXdliqJr9CGh1ZCLpoYbGP5o"
-auth_users = "6707073614, 1406300051, 6801378994, 870101201"
+api_id = 29768900
+api_hash = "6a56e5c2383dc7b85210febe60b9f229"
+bot_token = "7416646489:AAEYBOjSuR5nY5-wGBSXqJmI2foHhzjR4Jo"
+auth_users = "5986670447, 1406300051, 6801378994, 870101201"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6707073614"
+osowner_users = "5986670447"
 owner_users = [int(num) for num in osowner_users.split(",")]
