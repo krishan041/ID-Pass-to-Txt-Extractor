ac99028d11db5c5ea66f57894934e465d3b4ac13
krishan041 <krishandara04@gmail.com>
2025-01-30 13:28:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 29768900
-api_hash = "6a56e5c2383dc7b85210febe60b9f229"
-bot_token = "7416646489:AAEYBOjSuR5nY5-wGBSXqJmI2foHhzjR4Jo"
-auth_users = "5986670447, 1406300051, 6801378994, 870101201"
+api_id = 23442913
+api_hash = "864a97e16b4ff7dc65ff5e2d1549b4a2"
+bot_token = "7899433110:AAGbD4AWJOx9pFu-0pJubvB3uXKDuZEL7HE"
+auth_users = "6428531614, 7841326954"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "5986670447"
+osowner_users = "7841326954"
 owner_users = [int(num) for num in osowner_users.split(",")]
